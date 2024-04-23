84873e670a5ab5fb078c4ab86bf7709ad8148086
DoraHuang1234 <116648694+Alena0910@users.noreply.github.com>
2024-04-23 13:29:42 +0800

    可以變更方向

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-import java.security.SecurityPermission;
+import java.awt.event.*;
 import java.util.Random;
 
-public class SnakeGame extends JPanel{
+public class SnakeGame extends JPanel implements ActionListener, KeyListener{
     private class Tile {
         int x, y;
 
@@ -24,17 +24,29 @@ public class SnakeGame extends JPanel{
 
     Random random; // 建立 random 物件
 
+    // 讓遊戲不斷刷新
+    Timer gameloop;
+    int velocityX, velocityY;
+
     SnakeGame(int boardWidth, int boardHeight){
         this.boardHeight = boardHeight;
         this.boardWidth = boardWidth;
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.BLACK);
+        addKeyListener(this);
+        setFocusable(true);
 
         snakehead = new Tile((int)Math.sqrt(tileSize) , (int)Math.sqrt(tileSize) );
 
         fruit = new Tile(10, 10);
         random = new Random();
         placeFruit();
+
+        velocityX = 0;
+        velocityY = 1;
+
+        gameloop = new Timer(100, this); // 100 ms
+        gameloop.start();
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -60,4 +72,37 @@ public class SnakeGame extends JPanel{
         fruit.x = random.nextInt(boardWidth / tileSize);
         fruit.y = random.nextInt(boardWidth / tileSize);
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move(); // 更新蛇的位置
+        repaint(); // 不斷呼叫 draw 函式
+    }
+    public void move(){
+        // snakehead
+        snakehead.x += velocityX;
+        snakehead.y += velocityY;
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            velocityX = 0;
+            velocityY = -1;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            velocityX = 0;
+            velocityY = 1;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            velocityX = -1;
+            velocityY = 0;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            velocityX = 1;
+            velocityY = 0;
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {}
 }
